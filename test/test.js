4971522eefd093fdd006886155e16223ae660fef
Robert Collins <beau@collins.pub>
2017-06-05 12:29:39 -0700

    Tests non finite numbers

--- test/test.js
+++ test/test.js
@@ -291,4 +291,11 @@ describe('notepack', function () {
 
     expect(notepack.decode(notepack.encode(fixture))).to.deep.equal(fixture);
   });
+
+  [NaN, Infinity, -Infinity].forEach( function (value) {
+    it(value.toString(), function () {
+      expect(notepack.decode(notepack.encode(value))).to.equal(null);
+    });
+  });
+
 });
